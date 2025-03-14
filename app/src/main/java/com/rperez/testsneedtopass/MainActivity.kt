6a39589afd5707710fbc9bd1f38c8c7410adf2be
richa <richard19.perez77@yahoo.com>
2025-03-14 13:29:04 +0700

    update for no code yet in main

--- app/src/main/java/com/rperez/testsneedtopass/MainActivity.kt
+++ app/src/main/java/com/rperez/testsneedtopass/MainActivity.kt
@@ -6,13 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.rperez.testsneedtopass.ui.theme.TestsNeedToPassTheme
 
+/**
+ * Main activity
+ *
+ * @constructor Create empty Main activity
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        /**
+         * Start with compose show equation passed in
+         */
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TestsNeedToPassTheme {
-            }
+            TestsNeedToPassTheme {}
         }
     }
 }
